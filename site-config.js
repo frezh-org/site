277f27fc526edb8901aef044edf9d64db92d2747
Quốc Khánh <bkdev98@gmail.com>
2019-10-13 18:07:47 +0700

    initial

---
+++ site-config.js
@@ -0,0 +1,66 @@
+module.exports = {
+  // App Info
+  appstore_link         : "#",                                  // Enter App Store URL.
+  playstore_link        : "#",                                  // Enter Google Play Store URL.
+  google_analytics_ID   : "UA-121546163-5",                                        // Enter Google Analytics ID or ""
+  presskit_download_link: null,                                  // Enter a link to downloadable file or (e.g. public Dropbox link to a .zip file).
+  video_or_screenshot   : "screenshot",                                           // "screenshot" or "video"
+  app_url               : "https://frezh.org",                            // Domain of your website without path_prefix.
+  path_prefix           : "/",                                                    // Prefixes all links. For cases when deployed to example.github.io/automatic-gatsbyjs-app-landing-page/.
+  app_name              : "FREZH.ORG",
+  app_price             : "Air quality tracker",
+  app_description       : "",
+  app_keywords          : ["frezh", "frezh.org", "air quality", "pm25", "pollution"],
+
+  // Personal Info
+  your_name              : "Quoc Khanh",
+  your_link              : "https://buiquockhanh.com",
+  your_city              : "Sai Gon",
+  email_address          : "bkdev98@gmail.com",
+  facebook_username      : "bkdev98",
+  instagram_username     : "qk.6870",
+  twitter_username       : "bkdev98",
+  github_username        : "bkdev98",
+  youtube_username       : null,
+
+  // Features List
+  features               : [
+    {
+      title: "AQ tracker",
+      description:
+        "Track air quality data all over the world.",
+      fontawesome_icon_name: "map-marker-alt",
+    },
+    {
+      title: "Pollution map",
+      description:
+        "Latest measurements demonstrate in colors.",
+      fontawesome_icon_name: "globe-asia",
+    },
+    {
+      title: "Major city ranking",
+      description:
+        "List cities sorted by air pollution parameters.",
+      fontawesome_icon_name: "list-ol",
+    },
+  ],
+  header_background             : "rgba(0, 0, 0, 0.1)",
+  topbar_title_color            : "#ffffff",
+  cover_overlay_color_rgba      : "rgba(54, 59, 61, 0.8)",
+  device_color                  : "white",                          // Set to "black", "blue", "coral", "white", or "yellow"
+  body_background_color         : "#ffffff",
+  primary_text_color            : "#333333",
+  content_width                 : "1170px",
+  font                          : `"AmaticSC", sans-serif`,
+  link_color                    : "#FFCEC8",
+  app_title_color               : "#ffffff",
+  app_price_color               : "#ffffff",
+  app_description_color         : "#ffffff",
+  feature_title_color           : "#333333",
+  feature_text_color            : "#666666",
+  feature_icons_foreground_color: "#ffffff",
+  feature_icons_background_color: "#666666",
+  social_icons_foreground_color : "#666666",
+  social_icons_background_color : "#ffffff",
+  footer_text_color             : "#666666",
+}
